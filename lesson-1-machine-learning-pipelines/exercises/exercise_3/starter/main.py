b2681101b67b797d4caa3e7a6e78b9a5d8cb4e83
phidesigner <phi.designer@gmail.com>
2023-08-27 18:42:33 +0000

    my fork update

--- lesson-1-machine-learning-pipelines/exercises/exercise_3/starter/main.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_3/starter/main.py
@@ -1,8 +1,13 @@
 import mlflow
 import os
+import wandb
 import hydra
 from omegaconf import DictConfig
 
+# mlflow.set_tracking_uri("azureml://westeurope.api.azureml.ms/mlflow/v1.0/subscriptions/cdfebb73-07a2-4fa8-b753-62da76199209/resourceGroups/Udacity/providers/Microsoft.MachineLearningServices/workspaces/MLflows")
+mlflow.set_experiment("my_exp")
+
+# Your ML code and MLflow logging here
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
@@ -32,7 +37,16 @@ def go(config: DictConfig):
     # NOTE: use os.path.join(root_path, "process_data") to get the path
     # to the "process_data" component
     ##################
-
+    _ = mlflow.run(
+        os.path.join(root_path, "process_data"),
+        "main",
+        parameters={
+            "input_artifact": "iris.csv:latest",
+            "artifact_name": "cleaned_data.csv",
+            "artifact_type": "processed_data",
+            "artifact_description": "Cleaned data"
+        },
+    )
 
 
 if __name__ == "__main__":
